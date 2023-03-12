c87e77808c63ab2ddcb71f67b1b192c503a5dcb5
Som Akudu <somakudu@gmail.com>
2023-03-12 23:52:15 +0000

    install react router dom packages

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import Home from ".pages/Home"
+import Dashboard from ".pages/Dashboard"
+import OnBoarding from ".pages/Onboarding"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 const App = () => {
   return (
     <div>
