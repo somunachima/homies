1a547375ec257f525e548f9cd2dff31f4da4db15
Som Akudu <somakudu@gmail.com>
2023-03-13 00:18:44 +0000

    create components folder and Nav file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Home from ".pages/Home"
-import Dashboard from ".pages/Dashboard"
-import OnBoarding from ".pages/Onboarding"
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
+import OnBoarding from "./pages/OnBoarding"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const App = () => {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,18 @@
 const Home = () => {
-    return (
-        <div>Home</div>
+
+  const authToken = true
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+  <Nav/>
+  return (
+        <div className="home">
+          <h1>Swipe Right</h1>
+          <button className="home-btn" onClick={handleClick}>
+              {authToken ? 'Signout' : 'Create Account'}
+          </button>
+        </div>
     )
 }
 export default Home
