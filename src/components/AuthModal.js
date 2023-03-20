d5ef98c7e292c24e2912737a361af30d8f3944e2
Som Akudu <somakudu@gmail.com>
2023-03-20 22:28:18 +0000

    add form to the authmodal

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,11 +1,34 @@
+import { useState } from "react"
+
+
 const AuthModal = ({ setShowModal }) => {
 
   const handleClick = () => {
     setShowModal(false)
   }
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+  }
+
+  const isSignUp = true
+
   return (
       <div className="auth-modal">
-        <div onClick={handleClick}>ⓧ</div>
+        <div className="close-icon" onClick={handleClick}>ⓧ</div>
+        <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
+        <p>By clicking Log In, you agree to our terms. Learn how we process your data in our Privacy Policy and Cookie Policy.</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="email"
+            required={true}
+            onChange={{e} => setEmail(e.target.value)}
+          />
+        </form>
+
         AUTH MODAL
       </div>
   )
