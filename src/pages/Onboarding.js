4a9098ea380853afe53177a14d4d9c5324f41f08
Som Akudu <somakudu@gmail.com>
2023-03-22 17:42:03 +0000

    add form for profile details to onboarding page

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -25,16 +25,87 @@ const OnBoarding = () => {
                     onChange={handleChange}
                   />
 
-                  <label htmlFor="first_name">First Name</label>
+                  <label>Birthday</label>
+                  <div className="multiple-input-container">
                   <input
-                    id="first_name"
-                    type="text"
-                    name="first_name"
-                    placeholder="First Name"
+                    id="dob_day"
+                    type="number"
+                    name="dob_day"
+                    placeholder="DD"
                     required={true}
                     value={""}
                     onChange={handleChange}
                   />
+                  <input
+                    id="dob_month"
+                    type="number"
+                    name="dob_month"
+                    placeholder="MM"
+                    required={true}
+                    value={""}
+                    onChange={handleChange}
+                  />
+                  <input
+                    id="dob_year"
+                    type="number"
+                    name="dob_year"
+                    placeholder="YYYY"
+                    required={true}
+                    value={""}
+                    onChange={handleChange}
+                  />
+                  </div>
+
+                  <label>Gender</label>
+                  <div className="multiple-input-container">
+                  <input
+                    id="man-gender-identity"
+                    type="radio"
+                    name="gender-identity"
+                    placeholder="Man"
+                    value="man"
+                    onChange={handleChange}
+                    checked={false}
+                    />
+                    
+                  <input
+                    id="woman-gender-identity"
+                    type="radio"
+                    name="gender-identity"
+                    placeholder="Woman"
+                    value="woman"
+                    onChange={handleChange}
+                    checked={false}
+                    />
+                   <input
+                    id="transgender-identity"
+                    type="radio"
+                    name="gender-identity"
+                    placeholder="Non-Binary"
+                    value="transgender"
+                    onChange={handleChange}
+                    checked={false}
+                    />
+                  <input
+                    id="non-binary-identity"
+                    type="radio"
+                    name="gender-identity"
+                    placeholder="Non-Binary"
+                    value="non-binary"
+                    onChange={handleChange}
+                    checked={false}
+                    />
+                  <input
+                    id="more-gender-identity"
+                    type="radio"
+                    name="gender-identity"
+                    placeholder="More"
+                    value="more"
+                    onChange={handleChange}
+                    checked={false}
+                    />
+                  </div>
+
               </section>
           </form>
         </div>
