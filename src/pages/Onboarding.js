06d5772712392bcbe523697bd415b19013ec43a0
Som Akudu <somakudu@gmail.com>
2023-03-22 17:11:57 +0000

    add form for profile details to onboarding page

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -1,6 +1,44 @@
+import { useState } from "react"
+import Nav from "../components/Nav"
+
 const OnBoarding = () => {
   return (
-      <div>OnBoarding</div>
+      <>
+        <Nav
+            minimal={true}
+            setShowModal={() => []}
+            showModal={false}
+        />
+        <div className="onboarding">
+          <h2>CREATE ACCOUNT</h2>
+
+          <form onSubmit={handleSubmit}>
+              <section>
+                  <label htmlFor="first_name">First Name</label>
+                  <input
+                    id="first_name"
+                    type="text"
+                    name="first_name"
+                    placeholder="First Name"
+                    required={true}
+                    value={""}
+                    onChange={handleChange}
+                  />
+
+                  <label htmlFor="first_name">First Name</label>
+                  <input
+                    id="first_name"
+                    type="text"
+                    name="first_name"
+                    placeholder="First Name"
+                    required={true}
+                    value={""}
+                    onChange={handleChange}
+                  />
+              </section>
+          </form>
+        </div>
+      </>
   )
 }
 export default OnBoarding
