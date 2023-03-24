69cb6984758c804fd0706f787e91ab6b1193410c
Som Akudu <somakudu@gmail.com>
2023-03-24 14:52:32 +0000

    fix label input bug for gender indentity and gender interest on onboarding page

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -1,21 +1,48 @@
-import { useState } from "react"
 import Nav from "../components/Nav"
+import {useState} from "react"
 
 const OnBoarding = () => {
+  const [formData, setFormData] = useState({
+      user_id: "",
+      first_name: "",
+      dob_day: "",
+      dob_month: "",
+      dob_year: "",
+      show_gender: false,
+      gender_identity: "man",
+      gender_interest: "woman",
+      email: "",
+      location: "",
+      available_day: "",
+      available_month: "",
+      monthly_budget: "",
+      url: "",
+      about: "",
+      matches: []
+  })
 
   const handleSubmit = () => {
-      console.log("submitted")
+      console.log('submitted')
   }
 
-  const handleChange = () => {
-    console.log("change")
+  const handleChange = (e) => {
+    const value = e.target.type === "checkbox" ? e.target.checked : e.target.value
+    const name = e.target.name
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name] : value
+    }))
   }
+
+  console.log(formData)
 
   return (
       <>
         <Nav
             minimal={true}
-            setShowModal={() => []}
+            setShowModal={() => {
+            }}
             showModal={false}
         />
         <div className="onboarding">
@@ -30,7 +57,18 @@ const OnBoarding = () => {
                     name="first_name"
                     placeholder="First Name"
                     required={true}
-                    value={""}
+                    value={formData.first_name}
+                    onChange={handleChange}
+                  />
+
+                  <label htmlFor="location">Preferred Location to Live</label>
+                  <input
+                    id="location"
+                    type="text"
+                    name="location"
+                    placeholder="West Hampstead, London"
+                    required={true}
+                    value={formData.location}
                     onChange={handleChange}
                   />
 
@@ -42,7 +80,7 @@ const OnBoarding = () => {
                       name="dob_day"
                       placeholder="DD"
                       required={true}
-                      value={""}
+                      value={formData.dob_day}
                       onChange={handleChange}
                     />
                     <input
@@ -51,7 +89,7 @@ const OnBoarding = () => {
                       name="dob_month"
                       placeholder="MM"
                       required={true}
-                      value={""}
+                      value={formData.dob_month}
                       onChange={handleChange}
                     />
                     <input
@@ -60,7 +98,7 @@ const OnBoarding = () => {
                       name="dob_year"
                       placeholder="YYYY"
                       required={true}
-                      value={""}
+                      value={formData.dob_year}
                       onChange={handleChange}
                     />
                   </div>
@@ -70,62 +108,61 @@ const OnBoarding = () => {
                     <input
                       id="man-gender-identity"
                       type="radio"
-                      name="gender-identity"
-                      placeholder="Man"
+                      name="gender_identity"
                       value="man"
                       onChange={handleChange}
-                      checked={false}
+                      checked={formData.gender_identity === "man"}
                     />
                     <label htmlFor="man-gender-identity">Man</label>
+
                     <input
                       id="woman-gender-identity"
                       type="radio"
-                      name="gender-identity"
-                      placeholder="Woman"
+                      name="gender_identity"
                       value="woman"
                       onChange={handleChange}
-                      checked={false}
+                      checked={formData.gender_identity === "woman"}
                     />
                     <label htmlFor="woman-gender-identity">Woman</label>
+
                     <input
-                      id="transgender-identity"
+                      id="transgender-gender-identity"
                       type="radio"
-                      name="gender-identity"
-                      placeholder="Non-Binary"
+                      name="gender_identity"
                       value="transgender"
                       onChange={handleChange}
-                      checked={false}
+                      checked={formData.gender_identity === "transgender"}
                     />
-                    <label htmlFor="transgender-identity">Transgender</label>
+                    <label htmlFor="transgender-gender-identity">Transgender</label>
+
                     <input
-                      id="non-binary-identity"
+                      id="non-binary-gender-identity"
                       type="radio"
-                      name="gender-identity"
-                      placeholder="Non-Binary"
+                      name="gender_identity"
                       value="non-binary"
                       onChange={handleChange}
-                      checked={false}
+                      checked={formData.gender_identity === "non-binary"}
                     />
-                    <label htmlFor="non-binary-identity">Non-Binary</label>
+                    <label htmlFor="non-binary-gender-identity">Non-Binary</label>
+
                     <input
                       id="more-gender-identity"
                       type="radio"
-                      name="gender-identity"
-                      placeholder="More"
+                      name="gender_identity"
                       value="more"
                       onChange={handleChange}
-                      checked={false}
+                      checked={formData.gender_identity === "more"}
                     />
                     <label htmlFor="more-gender-identity">More</label>
                   </div>
 
-                  <label htmlFor="show-gender">Show gender on my profile</label>
+                  <label htmlFor="show-gender">Show Gender on my profile</label>
                   <input
                     id="show-gender"
                     type="checkbox"
                     name="show-gender"
                     onChange={handleChange}
-                    checked={false}
+                    checked={formData.show_gender}
                     />
 
                   <label>Show Me</label>
@@ -133,53 +170,85 @@ const OnBoarding = () => {
                   <input
                       id="man-gender-interest"
                       type="radio"
-                      name="gender-interest"
-                      placeholder="Man"
+                      name="gender_interest"
                       value="man"
                       onChange={handleChange}
-                      checked={false}
+                      checked={formData.gender_interest === "man"}
                     />
                     <label htmlFor="man-gender-interest">Man</label>
+
                     <input
                       id="woman-gender-interest"
                       type="radio"
-                      name="gender-interest"
-                      placeholder="Woman"
+                      name="gender_interest"
                       value="woman"
                       onChange={handleChange}
-                      checked={false}
+                      checked={formData.gender_interest === "woman"}
                     />
                     <label htmlFor="woman-gender-interest">Woman</label>
+
                     <input
-                      id="transgender-interest"
+                      id="transgender-gender-interest"
                       type="radio"
-                      name="gender-interest"
-                      placeholder="Non-Binary"
+                      name="gender_interest"
                       value="transgender"
                       onChange={handleChange}
-                      checked={false}
+                      checked={formData.gender_interest === "transgender"}
                     />
-                    <label htmlFor="transgender-interest">Transgender</label>
+                    <label htmlFor="transgender-gender-interest">Transgender</label>
+
                     <input
-                      id="non-binary-interest"
+                      id="non-binary-gender-interest"
                       type="radio"
-                      name="gender-interest"
-                      placeholder="Non-Binary"
+                      name="gender_interest"
                       value="non-binary"
                       onChange={handleChange}
-                      checked={false}
+                      checked={formData.gender_interest === "non-binary"}
                     />
-                    <label htmlFor="non-binary-interest">Non-Binary</label>
+                    <label htmlFor="non-binary-gender-interest">Non-Binary</label>
+
                     <input
                       id="everyone-gender-interest"
                       type="radio"
-                      name="gender-interest"
-                      placeholder="More"
-                      value="more"
+                      name="gender_interest"
+                      value="everyone"
                       onChange={handleChange}
-                      checked={false}
+                      checked={formData.gender_interest === "everyone"}
                     />
-                    <label htmlFor="more-gender-interest">Everyone</label>
+                    <label htmlFor="everyone-gender-interest">Everyone</label>
+                  </div>
+
+                  <label htmlFor="monthly_budget">Monthly Budget (£)</label>
+                  <input
+                    id="monthly_budget"
+                    type="number"
+                    name="monthly_budget"
+                    required={true}
+                    placeholder="850"
+                    value={formData.monthly_budget}
+                    onChange={handleChange}
+                  />
+
+                  <label>Moving Date</label>
+                  <div className="multiple-input-container">
+                    <input
+                      id="available_day"
+                      type="number"
+                      name="available_day"
+                      placeholder="DD"
+                      required={true}
+                      value={formData.available_day}
+                      onChange={handleChange}
+                    />
+                    <input
+                      id="available_month"
+                      type="number"
+                      name="available_month"
+                      placeholder="MM"
+                      required={true}
+                      value={formData.available_month}
+                      onChange={handleChange}
+                    />
                   </div>
 
                   <label htmlFor="about">About</label>
@@ -189,7 +258,7 @@ const OnBoarding = () => {
                     name="about"
                     required={true}
                     placeholder="I like going for long walks..."
-                    value={""}
+                    value={formData.about}
                     onChange={handleChange}
                   />
                   <input type="submit"/>
@@ -205,7 +274,7 @@ const OnBoarding = () => {
                   required={true}
                 />
                 <div className="pic-container">
-
+                  <img src={formData.url} alt="profile pic preview"/>
                 </div>
               </section>
           </form>
