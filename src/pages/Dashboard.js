7113d7a8ac1c1f016853229405307c0a3e584e92
Som Akudu <somakudu@gmail.com>
2023-03-24 17:33:33 +0000

    begin adding details and structure to dashboard and install npm react tinder card package

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,11 @@
 const Dashboard = () => {
   return (
-      <div>Dashboard</div>
+      <div className="dashboard">
+          <ChatContainer/>
+          <div className="swiper-container">
+            <div className="card-container"></div>
+          </div>
+      </div>
   )
 }
 export default Dashboard
