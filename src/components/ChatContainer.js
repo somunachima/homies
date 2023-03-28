608999cc681acda6cfc25ddf33e329d88628878a
Som Akudu <somakudu@gmail.com>
2023-03-28 23:23:19 +0100

    create html  structure for chatroom

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -1,6 +1,21 @@
 import { useState } from "react";
 
 const ChatContainer = () => {
-  return <div>ChatContainer</div>
+    return (
+        <div className="chat-container">
+          <ChatHeader/>
+
+            <div>
+              <button className="option">Matches</button>
+              <button className="option">Chat</button>
+            </div>
+
+          <MatchesDisplay/>
+
+          
+        </div>
+
+
+    )
 }
 export default ChatContainer
