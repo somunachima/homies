09caa5287ec7715056de10bf781b429ccc14ca2f
Som Akudu <somakudu@gmail.com>
2023-04-04 23:48:05 +0100

    connect the sign up page to the backend

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const PORT = 8000
-
 const express = require('express')
+const {MongoClient} = required('mongodb')
+const uri
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.json('Hello to my app')
+})
+
+app.get('/signup', (req, res) => {
+  res.json('Hello to my app')
+
 })
 
 app.listen(PORT, () => console.log('Server running on PORT ' + PORT))
