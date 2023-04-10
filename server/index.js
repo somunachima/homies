24d38409650352b907320c029a7386e4abc98f2c
Som Akudu <somakudu@gmail.com>
2023-04-10 23:13:13 +0100

    add uri with mongodb database and password

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const PORT = 8000
 const express = require('express')
-const {MongoClient} = require('mongodb')
-const uri = 'mongodb+srv://somakudu:thepassword@cluster.cixiijx.mongodb.net/?retryWrites=true&w=majority'
+const {MongoClient, ServerApiVersion} = require('mongodb')
+const uri = "mongodb+srv://somakudu:homies2023@cluster.cixiijx.mongodb.net/?retryWrites=true&w=majority"
 
 const app = express()
 
