efd6afb5b6b995eaaa2df678796d94e67f0791ff
Som Akudu <somakudu@gmail.com>
2023-04-10 22:29:49 +0100

    return to code where there is no error on localhost 8000

--- server/index.js
+++ server/index.js
@@ -1,17 +1,10 @@
 const PORT = 8000
 const express = require('express')
-const {MongoClient} = required('mongodb')
-const uri
+const {MongoClient} = require('mongodb')
+const uri = 'mongodb+srv://somakudu:thepassword@cluster.cixiijx.mongodb.net/?retryWrites=true&w=majority'
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.json('Hello to my app')
-})
 
-app.get('/signup', (req, res) => {
-  res.json('Hello to my app')
-
-})
 
 app.listen(PORT, () => console.log('Server running on PORT ' + PORT))
