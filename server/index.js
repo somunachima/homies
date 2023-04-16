2b873c1074cc593dca5ddc6e11a4aad95643356d
Som Akudu <somakudu@gmail.com>
2023-04-16 23:18:20 +0100

    connect the sign up form to the database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const PORT = 8000
 const express = require('express')
 const {MongoClient} = require('mongodb')
+const {v1: uuidv4} = require('uuid')
 const uri = "mongodb+srv://somakudu:homies2023@cluster.cixiijx.mongodb.net/?retryWrites=true&w=majority"
 
 const app = express()
@@ -9,9 +10,26 @@ app.get('/', (req, res) => {
     res.json('Hello to my app')
 })
 
-app.post('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
     const client = new MongoClient(uri)
-    
+    const {email, password} = req.body
+
+    const generatedUserId = uuidv4()
+    const hashPassword = await bcrypt.hash(password, 10)
+
+    try {
+        client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const existingUser = users.findOne({email})
+
+        if (existingUser) {
+            return res.status(409).send('User already exists. Please login')
+        }
+
+        email.toLowerCase()
+    }
 
 })
 
